f22057c706b86ce178dd2ba0f58101bb9dc77173
IGOR IBIAPINO DOS SANTOS <igor.isantos1@senacsp.edu.br>
2025-08-27 09:37:02 -0300

    ex11

---
+++ lista-exercicio/ex11/index.js
@@ -0,0 +1,12 @@
+let num1, num2
+
+num1 = prompt("digite o primeiro numero: ")
+num2 = prompt("digite o segundo numero: ")
+
+if(num1 > num2){
+    alert("O primeiro numero é maior que o segundo")
+} else if(num1 == num2){
+    alert("Os dois numeros são iguais")
+} else {
+    ("O segundo numero é maior que o primeiro")
+}
