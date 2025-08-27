e6d8bd987f832f5e0d6b9fff0d8b28fce24dac6f
IGOR IBIAPINO DOS SANTOS <igor.isantos1@senacsp.edu.br>
2025-08-27 08:39:22 -0300

    ex3

---
+++ lista-exercicio/ex3/index.js
@@ -0,0 +1,12 @@
+let idade1
+let idade2
+idade1 = prompt("Qual a primeira idade? ")
+idade2 = prompt("Qual a segunda idade? ")
+
+if (idade1 > idade2) {
+    alert("A primeira pessoa é mais velha")
+} else if (idade1 = idade2) {
+    alert("As duas pessoas tem a mesma idade")
+} else {
+    alert("A segunda pessoa é mais velha")
+}
