e75c4a1c8764dacfd3dd97e1373f463f5fe17115
IGOR IBIAPINO DOS SANTOS <igor.isantos1@senacsp.edu.br>
2025-08-27 09:25:38 -0300

    ex10

---
+++ lista-exercicio/ex10/index.js
@@ -0,0 +1,16 @@
+let menu
+menu = prompt("Oq deseja ver? 1-saldo, 2-depositar, 3-sair")
+switch (menu) {
+    case 1:
+        alert("Seu saldo é de R$ 1.000.000.000,00")
+        break;
+    case 2:
+        alert("Seu deposito é de R$ 1.000.000.000,00")
+        break;
+    case 3:
+        alert("vc saiu da sua conta")
+        break;
+
+    default:
+        break;
+}
