def89de6751d714804a370511ff8902c24e7cb2d
IGOR IBIAPINO DOS SANTOS <igor.isantos1@senacsp.edu.br>
2025-08-27 09:19:35 -0300

    ex9

---
+++ lista-exercicio/ex9/index.js
@@ -0,0 +1,10 @@
+let nota
+nota = prompt("Qual foi a sua nota? ")
+
+if (nota > 7){
+    alert("vc foi aprovado")
+} else if (nota < 7 && nota >= 5) {
+    alert("vc esta de recuperação") }
+else {
+    alert("vc esta reprovado")
+}
