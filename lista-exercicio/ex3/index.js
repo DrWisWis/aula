cec030709b49b5ddb6426826b92f57ef579b5e22
IGOR IBIAPINO DOS SANTOS <igor.isantos1@senacsp.edu.br>
2025-08-27 08:53:07 -0300

    ex4

--- lista-exercicio/ex3/index.js
+++ lista-exercicio/ex3/index.js
@@ -5,7 +5,7 @@ idade2 = prompt("Qual a segunda idade? ")
 
 if (idade1 > idade2) {
     alert("A primeira pessoa é mais velha")
-} else if (idade1 = idade2) {
+} else if (idade1 == idade2) {
     alert("As duas pessoas tem a mesma idade")
 } else {
     alert("A segunda pessoa é mais velha")
---
+++ lista-exercicio/ex4/index.js
@@ -0,0 +1,9 @@
+let num
+
+num = prompt("Escreva um numero: ")
+
+if(num%2 == 0) {
+    alert("O numero ",num ," é par")
+} else {
+    alert("O numero ",num ," é impar")
+}
