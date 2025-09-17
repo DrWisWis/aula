aa78c4250de7bdb640ee5726c09391225aa284dd
DrWisWis <ibiapinoigor@gmail.com>
2025-09-17 11:14:55 -0300

    ex5

---
+++ lista-exercicio2/ex5/index.js
@@ -0,0 +1,8 @@
+function multiplicacao(a) {
+    resultado = a * a
+    return alert(`O dobro deste numero é ${resultado}`)
+}
+
+num1 = Number(prompt("Escreva o numero que vai dobrar: "))
+
+multiplicacao(num1)
