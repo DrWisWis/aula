d4da4cad842f1710dbbc8887ba54c0a4e21a2f2e
DrWisWis <ibiapinoigor@gmail.com>
2025-09-17 11:12:43 -0300

    ex4

---
+++ lista-exercicio2/ex4/index.js
@@ -0,0 +1,9 @@
+function multiplicacao(a, b) {
+    resultado = a * b
+    return alert(`O resulta de ${a} x ${b} = ${resultado}`)
+}
+
+num1 = Number(prompt("Escreva o primeiro numero"))
+num2 = Number(prompt("Escreva o segundo numero"))
+
+multiplicacao(num1, num2)
