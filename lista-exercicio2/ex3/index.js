21de564928cae86f9505de2bba82d6efcd4546ac
DrWisWis <ibiapinoigor@gmail.com>
2025-09-17 11:10:43 -0300

    ex3

---
+++ lista-exercicio2/ex3/index.js
@@ -0,0 +1,9 @@
+function soma(a, b) {
+    resultado = a + b
+    return alert(`O resulta de ${a} + ${b} = ${resultado}`)
+}
+
+num1 = Number(prompt("Escreva o primeiro numero"))
+num2 = Number(prompt("Escreva o segundo numero"))
+
+soma(num1, num2)
