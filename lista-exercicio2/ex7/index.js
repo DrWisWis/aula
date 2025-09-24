b2361726af3a0684582270f0467fd7d45efaae79
DrWisWis <ibiapinoigor@gmail.com>
2025-09-24 09:29:26 -0300

    ex7

---
+++ lista-exercicio2/ex7/index.js
@@ -0,0 +1,13 @@
+function maior(a, b) {
+    if(a > b){
+        alert("O primeiro valor é maior que o segundo")
+    } else {
+        alert("O segundo valor é maior que o primeiro")
+    }
+}
+
+a = prompt("Escreva um numero: ")
+
+b = prompt("Escreva o segundo numero: ")
+
+maior(a, b)
