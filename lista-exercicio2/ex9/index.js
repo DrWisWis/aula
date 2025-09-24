1dbe602ab1effad5f4d278f8cdf5a2f9c6501f39
DrWisWis <ibiapinoigor@gmail.com>
2025-09-24 10:23:00 -0300

    ex9

---
+++ lista-exercicio2/ex9/index.js
@@ -0,0 +1,11 @@
+function media(a, b, c) {
+    let media = 0
+    media = (a + b + c)/3
+    alert(`A sua media é de ${media}`)
+}
+
+num1 = Number(prompt("Escreva a sua primeira nota: "))
+num2 = Number(prompt("Escreva a sua primeira nota: "))
+num3 = Number(prompt("Escreva a sua primeira nota: "))
+
+media(num1, num2, num3)
